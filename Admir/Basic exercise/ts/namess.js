bf8f175ebfa5e15ebf30006c88f9b975ae8f1baa
Admir Saraseli <asaraseli@gmail.com>
2020-06-17 13:13:04 +0200

    basic ex

--- Admir/Basic exercise/ts/namess.js
+++ Admir/Basic exercise/ts/namess.js
@@ -1,9 +1,14 @@
 var namedata = ['Admir', 'Bruno', 'Fasy',];
-for (var prop in namedata) {
-    console.log(namedata[prop]);
-}
-;
 for (var index in namedata) {
     console.log(index);
+}
+;
+for (var _i = 0, namedata_1 = namedata; _i < namedata_1.length; _i++) {
+    var value = namedata_1[_i];
+    console.log(value);
+}
+;
+for (var el in namedata) {
+    console.log(el, namedata[el]);
 }
 ;
--- Admir/Basic exercise/ts/namess.ts
+++ Admir/Basic exercise/ts/namess.ts
@@ -1,12 +1,15 @@
 
 let namedata: string[] = ['Admir', 'Bruno', 'Fasy',];
 
-
-for (let prop in namedata) {
-   console.log(namedata[prop]);
-};
-
 for (let index in namedata) {
    console.log(index);
 };
 
+for (let value of namedata) {
+   console.log(value);
+};
+
+
+for (let el in namedata) {
+   console.log(el, namedata[el]);
+};
